1ecbf73f1b800e7b20d5bc11fd845b560124cc2e
VISHAL V NAIR <vishalvnair124@gmail.com>
2024-08-26 11:55:38 +0530

    web and phone version one

--- lib/screens/course_list_screen.dart
+++ lib/screens/course_list_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:resource_hub/routes/fade_transition_route.dart';
 import 'package:resource_hub/screens/module_list_screen.dart';
-import 'package:resource_hub/screens/search_screen.dart'; // Import SearchScreen
+import 'package:resource_hub/screens/search_screen.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 import '../models/course.dart';
 import '../services/api_service.dart';
@@ -66,142 +66,162 @@ class _CourseListScreenState extends State<CourseListScreen> {
               coursesBySem[course.semester]!.add(course);
             }
 
-            return ListView.builder(
-              itemCount: coursesBySem.keys.length,
-              itemBuilder: (context, semIndex) {
-                final semester = coursesBySem.keys.elementAt(semIndex);
-                final semesterCourses = coursesBySem[semester]!;
+            return LayoutBuilder(
+              builder: (context, constraints) {
+                final isLargeScreen = constraints.maxWidth > 600;
 
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Semester $semester',
-                        style: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      ListView.builder(
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemCount: semesterCourses.length,
-                        itemBuilder: (context, index) {
-                          final course = semesterCourses[index];
-                          return Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  FadeTransitionRoute(
-                                    page: ModuleListScreen(courseId: course.id),
-                                  ),
-                                );
-                              },
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: NetworkImage(
-                                      course.linkImage,
-                                    ),
-                                    fit: BoxFit.cover,
-                                  ),
-                                  border: Border.all(color: Colors.black),
-                                  borderRadius: BorderRadius.circular(50),
-                                ),
-                                height:
-                                    MediaQuery.of(context).size.height * 0.275,
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Container(
-                                      decoration: BoxDecoration(
-                                        border: Border.all(color: Colors.black),
-                                        color: const Color.fromARGB(
-                                            255, 33, 149, 243),
-                                        borderRadius: const BorderRadius.only(
-                                          bottomLeft: Radius.circular(50),
-                                          bottomRight: Radius.circular(50),
-                                        ),
-                                      ),
-                                      height:
-                                          MediaQuery.of(context).size.height *
-                                              0.12,
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Expanded(
-                                            child: Padding(
-                                              padding: const EdgeInsets.only(
-                                                  left: 30),
-                                              child: Text(
-                                                course.name,
-                                                style: const TextStyle(
-                                                  fontSize: 24,
-                                                  fontWeight: FontWeight.w500,
-                                                ),
-                                                overflow: TextOverflow.ellipsis,
-                                              ),
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.only(
-                                                right: 15, top: 6, bottom: 6),
-                                            child: SizedBox(
-                                              width: 100,
-                                              height: 100,
-                                              child: SfRadialGauge(
-                                                axes: [
-                                                  RadialAxis(
-                                                    startAngle: 270,
-                                                    endAngle: 270,
-                                                    showLabels: false,
-                                                    showTicks: false,
-                                                    pointers: const [
-                                                      RangePointer(
-                                                        color: Color.fromARGB(
-                                                            255, 5, 225, 241),
-                                                        value: 75,
-                                                      ),
-                                                    ],
-                                                    annotations: const [
-                                                      GaugeAnnotation(
-                                                        widget: Text(
-                                                          "100%",
-                                                          style: TextStyle(
-                                                            color: Colors.white,
-                                                            fontSize: 20,
-                                                            fontWeight:
-                                                                FontWeight.w700,
-                                                          ),
-                                                        ),
-                                                      ),
-                                                    ],
-                                                  ),
-                                                ],
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
+                return ListView.builder(
+                  itemCount: coursesBySem.keys.length,
+                  itemBuilder: (context, semIndex) {
+                    final semester = coursesBySem.keys.elementAt(semIndex);
+                    final semesterCourses = coursesBySem[semester]!;
+
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Semester $semester',
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
                             ),
-                          );
-                        },
+                          ),
+                          isLargeScreen
+                              ? GridView.builder(
+                                  physics: NeverScrollableScrollPhysics(),
+                                  shrinkWrap: true,
+                                  gridDelegate:
+                                      SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount:
+                                        3, // Number of columns on larger screens
+                                    childAspectRatio: 3 /
+                                        2, // Adjust the aspect ratio as needed
+                                  ),
+                                  itemCount: semesterCourses.length,
+                                  itemBuilder: (context, index) {
+                                    return courseCard(semesterCourses[index]);
+                                  },
+                                )
+                              : ListView.builder(
+                                  physics: NeverScrollableScrollPhysics(),
+                                  shrinkWrap: true,
+                                  itemCount: semesterCourses.length,
+                                  itemBuilder: (context, index) {
+                                    return courseCard(semesterCourses[index]);
+                                  },
+                                ),
+                        ],
                       ),
-                    ],
-                  ),
+                    );
+                  },
                 );
               },
             );
           }
         },
+      ),
+    );
+  }
+
+  Widget courseCard(Course course) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.of(context).push(
+            FadeTransitionRoute(
+              page: ModuleListScreen(courseId: course.id),
+            ),
+          );
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                course.linkImage,
+              ),
+              fit: BoxFit.cover,
+              onError: (_, __) {
+                print('Image not loaded');
+              },
+            ),
+            border: Border.all(color: Colors.black),
+            borderRadius: BorderRadius.circular(50),
+          ),
+          height: MediaQuery.of(context).size.height * 0.275,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black),
+                  color: const Color.fromARGB(255, 33, 149, 243),
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                ),
+                height: MediaQuery.of(context).size.height * 0.12,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 30),
+                        child: Text(
+                          course.name,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(right: 15, top: 6, bottom: 6),
+                      child: SizedBox(
+                        width: 100,
+                        height: 100,
+                        child: SfRadialGauge(
+                          axes: [
+                            RadialAxis(
+                              startAngle: 270,
+                              endAngle: 270,
+                              showLabels: false,
+                              showTicks: false,
+                              pointers: const [
+                                RangePointer(
+                                  color: Color.fromARGB(255, 5, 225, 241),
+                                  value: 75,
+                                ),
+                              ],
+                              annotations: const [
+                                GaugeAnnotation(
+                                  widget: Text(
+                                    "100%",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
